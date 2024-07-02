4cca76890350b3a180facf961176e8a527b62496
vitor.miranda <vitor.miranda@ifood.com.br>
2024-07-01 21:47:47 -0300

    feat: adding validations

--- src/campaigns/campaigns.service.ts
+++ src/campaigns/campaigns.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Campaign } from 'src/model/campaign.entity';
 import { Repository } from 'typeorm';
@@ -10,14 +10,26 @@ export class CampaignsService {
     constructor(
         @InjectRepository(Campaign)
         private readonly campaignRepository: Repository<Campaign>,
-    ) {}
-
-    async create(createCampaignDto: CreateCampaignDto) {
+    ) { }
+    async create(createCampaignDto: CreateCampaignDto): Promise<Campaign> {
+        
         const currentDate = new Date();
+
+        const isEndDateValid =new Date(createCampaignDto.dataFim) > new Date(createCampaignDto.dataInicio)
+        const isStartDateValid =new Date(createCampaignDto.dataInicio) > currentDate
+
+        if (isEndDateValid) {
+            throw new HttpException('The end date cannot be in the past', HttpStatus.PRECONDITION_FAILED);
+        }
+
+        if (isStartDateValid) {
+            throw new HttpException('The start date cannot be in the past', HttpStatus.PRECONDITION_FAILED);
+        }
+
         if (new Date(createCampaignDto.dataFim) < currentDate) {
             createCampaignDto.status = 'expirada';
         }
-    
+
         const campaign = this.campaignRepository.create(createCampaignDto);
         return this.campaignRepository.save(campaign);
     }
@@ -31,14 +43,14 @@ export class CampaignsService {
         if (!campaign) {
             throw new Error('Campaign not found');
         }
-    
+
         const currentDate = new Date();
         if (updateCampaignDto.dataFim && new Date(updateCampaignDto.dataFim) < currentDate) {
             updateCampaignDto.status = 'expirada';
         } else if (updateCampaignDto.dataInicio && new Date(updateCampaignDto.dataInicio) < currentDate) {
             throw new Error('Start date must be today or later');
         }
-    
+
         return this.campaignRepository.update(id, updateCampaignDto);
     }
 
